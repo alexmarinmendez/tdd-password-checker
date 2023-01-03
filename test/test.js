f26b9664e9795770c80fcee605163637f1f7cd31
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-03 11:13:14 -0500

    feat: add hasUpperCase verification

--- test/test.js
+++ test/test.js
@@ -1,1 +1,6 @@
-console.log('TDD')
+import assert from 'assert'
+import { PasswordChecker } from "../src/main.js"
+
+let password = 'A'
+let pc = new PasswordChecker(password);
+assert.strictEqual(pc.hasUpperCase(), true)
