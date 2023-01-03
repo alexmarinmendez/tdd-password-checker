0f3fc767140702328920d0f7b3a508fba369c9fa
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-03 11:15:32 -0500

    feat: add test2 for hasUpperCase verification

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 export class PasswordChecker {
-    
+    hasUpperCase() {
+        return true
+    }
 }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,17 @@
 import assert from 'assert'
 import { PasswordChecker } from "../src/main.js"
 
-let password = 'A'
-let pc = new PasswordChecker(password);
-assert.strictEqual(pc.hasUpperCase(), true)
+const test1 = () => {
+    let password = 'A'
+    let pc = new PasswordChecker(password);
+    assert.strictEqual(pc.hasUpperCase(), true)
+}
+
+const test2 = () => {
+    let password = 'a'
+    let pc = new PasswordChecker(password);
+    assert.strictEqual(pc.hasUpperCase(), false)
+}
+
+test1()
+test2()
